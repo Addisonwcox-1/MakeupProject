bb7a16f6da5a28379e038ee4474888a8554231da
Addie Cox <Addie Cox@DESKTOP-NF4CRLO>
2019-04-24 17:46:27 -0700

    Fix images

---
+++ Makeup Homework 1 Solution/Makeup Homework 1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Makeup_Homework_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            new Image as img;
+
+            var uri = new Uri("https://food.fnr.sndimg.com/content/dam/images/food/video/0/02/023/0238/0238752.jpg.rend.hgtvcom.616.347.suffix/1480464986868.jpeg");
+            img.Source = new BitmapImage(uri);
+        }
+    }
+}
